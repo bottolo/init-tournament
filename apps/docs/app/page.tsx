be905bf32ad3dba05c3caad90f171f9b6183226e
bottolo <samuele.castiglia.98@gmail.com>
2025-06-08 20:35:45 +0200

    feat: added tailwind to applications

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
           priority
         />
         <ol>
-          <li>
+          <li className={"bg-blue-500"}>
             Get started by editing <code>apps/docs/app/page.tsx</code>
           </li>
           <li>Save and see your changes instantly.</li>
